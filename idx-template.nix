9a81970c873d088f23a50fcfda3c5b16440fa2f5
Cali (Renato Caliari) <renatoac82@gmail.com>
2024-12-17 09:50:46 -0300

    Update idx-template.nix

--- idx-template.nix
+++ idx-template.nix
@@ -1,17 +1,31 @@
 # No user-configurable parameters
 { pkgs, ... }: {
-  # Shell script that produces the final environment
+  packages = [
+    # Enable "node", "npm" and "npx" in the bootstrap script below.
+    # Note, this is NOT the list of packages available to the workspace once
+    # it's created. Those go in .idx/dev.nix
+    pkgs.nodejs
+  ];
+
+# Shell script that produces the final environment
   bootstrap = ''
+    npm i sv
+    npx sv create "$out" --template minimal --types ts --no-add-ons --no-install
+    cd "$out"
+    npx sv add prettier eslint tailwind --tailwindcss none --sveltekit-adaptar vercel   
+    npm install
+    git init && git add -A && git commit -m "Initial commit"
+
     # Copy the folder containing the `idx-template` files to the final
     # project folder for the new workspace. ${./.} inserts the directory
     # of the checked-out Git folder containing this template.
-    cp -rf ${./.} "$out"
+    # cp -rf ${./.} "$out"
 
     # Set some permissions
-    chmod -R +w "$out"
+    # chmod -R +w "$out"
 
     # Remove the template files themselves and any connection to the template's
     # Git repository
-    rm -rf "$out/.git" "$out/idx-template".{nix,json}
+    # rm -rf "$out/.git" "$out/idx-template".{nix,json}
   '';
 }
