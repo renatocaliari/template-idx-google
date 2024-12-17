6421ecdc7815c1afe1ccb9089772f24468e005f6
Cali (Renato Caliari) <renatoac82@gmail.com>
2024-12-17 10:02:33 -0300

    Update idx-template.nix

--- idx-template.nix
+++ idx-template.nix
@@ -4,8 +4,58 @@
     # Enable "node", "npm" and "npx" in the bootstrap script below.
     # Note, this is NOT the list of packages available to the workspace once
     # it's created. Those go in .idx/dev.nix
-    pkgs.nodejs
+    pkgs.nodejs_20
+    pkgs.python312
+    pkgs.python312Packages.pip
+    pkgs.python312Packages.pipx
+    pkgs.gcc
+    pkgs.stdenv.cc.cc.lib
   ];
+
+  # Sets environment variables in the workspace
+  env = {
+        # GEMINI_API_KEY="{YOUR API KEY}";
+        LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+  };
+
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "ms-vscode.live-server"
+      "svelte.svelte-vscode"
+      "saoudrizwan.claude-dev"
+      "Blackboxapp.blackbox"
+    ];
+
+  previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--host" "0.0.0.0"];
+          manager = "web";
+        };
+      };
+    };
+
+ # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        npm-install = "npm install";
+        install-aider-chat = "pipx install aider-chat";
+        # run-aider-chat = "pipx run aider-chat";
+        # Open editors for the following files by default, if they exist:
+        # default.openFiles = [ ".idx/dev.nix" "README.md" ];
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        # watch-backend = "npm run watch-backend";
+        # install-aider-chat = "pipx install aider-chat";
+        # run-aider-chat = "pipx run aider-chat";
+      };
+    };
 
 # Shell script that produces the final environment
   bootstrap = ''
