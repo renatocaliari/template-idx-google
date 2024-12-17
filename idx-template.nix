c1d5a87630c7f5085f10fd27c67ab139b5348138
Cali (Renato Caliari) <renatoac82@gmail.com>
2024-12-17 10:27:43 -0300

    Update idx-template.nix

--- idx-template.nix
+++ idx-template.nix
@@ -8,43 +8,11 @@
     # pkgs.nodejs
   ];
 
-  idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [
-      "ms-vscode.live-server"
-      "svelte.svelte-vscode"
-      "saoudrizwan.claude-dev"
-      "Blackboxapp.blackbox"
-    ];
-
-    previews = {
-      enable = true;
-      previews = {
-        web = {
-          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--host" "0.0.0.0"];
-          manager = "web";
-        };
-      };
-    };
-
-    workspace = {
-      # Runs when a workspace is first created
-      onCreate = {
-        # Example: install JS dependencies from NPM
-        npm-install = "npm install";
-        # Open editors for the following files by default, if they exist:
-        # default.openFiles = [ ".idx/dev.nix" "README.md" ];
-      };
-      # Runs when the workspace is (re)started
-      onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
-      };
-    };
-  };
-
 # Shell script that produces the final environment
   bootstrap = ''
+    cp -rf ${./.} "$out"
+    chmod -R +w "$out"
+    rm -rf "$out/.git" "$out/idx-template".{nix,json}
     npm i sv
     npx sv create "$out" --template minimal --types ts --no-add-ons --no-install
     cd "$out"
